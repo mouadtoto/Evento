34145bb45418e7938aaba67fcc24e9866e6a5be0
mars01253 <mauadtoto001@gmail.com>
2024-03-04 17:30:30 +0100

    migrations

--- database/migrations/2024_03_04_150832_create_organizers_table.php
+++ database/migrations/2024_03_04_150832_create_organizers_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('role');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('isRestricted')->default(0);
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('logo');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizers');
     }
 };
---
+++ database/migrations/2024_03_04_151154_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('participant_id');
+            $table->foreignId('participant_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('event_id');
+            $table->foreignId('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
